39185f5e3730094d3800978f2e66a0c9831ec7d8
Admin <sandhyabagale10@gmail.com>
2021-10-28 14:25:59 +0530

    added:Password by using special Character

--- src/test/java/bridgelabz/User_Registration_pgrm/UserRegistrationTest.java
+++ src/test/java/bridgelabz/User_Registration_pgrm/UserRegistrationTest.java
@@ -102,4 +102,16 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.passwordRule3("Sandhya");
 		Assert.assertEquals(false, result);
 	}
+
+	@Test
+	public void givenPasswordRule4_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.passwordRule4("Sandhya@22");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPasswordRule4_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.passwordRule4("Sandhya");
+		Assert.assertEquals(false, result);
+	}
 }
