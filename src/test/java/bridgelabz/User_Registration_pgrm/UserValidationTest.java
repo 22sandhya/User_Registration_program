12b50713e5e8d06cda188e7b3d17e656048e7abe
Admin <sandhyabagale10@gmail.com>
2021-10-28 15:14:29 +0530

    Added:Throw Coustom Exception

---
+++ src/test/java/bridgelabz/User_Registration_pgrm/UserValidationTest.java
@@ -0,0 +1,64 @@
+package bridgelabz.User_Registration_pgrm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.User_Registration_pgrm.UserValidation;
+import com.bridgelabz.User_Registration_pgrm.UserValidationException;
+
+public class UserValidationTest {
+    @Test
+    public void givenFirstName_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = true;
+        try {
+            actualResult = validation.firstName("Manish");
+        } catch (UserValidationException exception) {
+            Assert.assertEquals(actualResult, exception.getMessage());
+        }
+    }
+
+    @Test
+    public void lastName_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = true;
+        try {
+            actualResult = validation.lastName("Kumar");
+        } catch (UserValidationException exception) {
+            Assert.assertEquals(actualResult, exception.getMessage());
+        }
+    }
+
+    @Test
+    public void email_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = true;
+        try {
+            actualResult = validation.email("mkaubr007@gmail.com");
+        } catch (UserValidationException exception) {
+            Assert.assertEquals(actualResult, exception);
+        }
+    }
+
+    @Test
+    public void phoneNumber_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = true;
+        try {
+            actualResult = validation.phoneNumber("+918002695700");
+        } catch (UserValidationException exception) {
+            Assert.assertEquals(actualResult, exception.getMessage());
+        }
+    }
+
+    @Test
+    public void password_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult=true;
+        try {
+            actualResult = validation.password("qw1rt@Ms");
+        }catch (UserValidationException exception) {
+            Assert.assertEquals(actualResult,exception.getMessage());
+        }
+    }
+}
